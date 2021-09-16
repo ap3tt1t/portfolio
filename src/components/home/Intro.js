7de590fb943121667b350b76d32ee55febe9e813
Andrew Pettit <pettit.andrew@gmail.com>
2021-09-16 12:49:30 +0200

    Fixed spelling errors and home page - intro comp

--- src/components/home/Intro.js
+++ src/components/home/Intro.js
@@ -1,13 +1,13 @@
 const Intro = () => (
     <div className='col-12 surface-card p-4 shadow-2 border-round'>
         <p>
-        I am a self-taught software developer speciailising in React and Node.js 
+        I am a self-taught software developer specialising in React and Node.js 
         with two years of personal coding experience.
         I learned to code using various courses on Udemy and YouTube, and I have built several side
         projects to gain practical experience.
-        Last year I built and ERP (Enterprise Resource Planning) for my company's branch that I have
-        used to automate inventory, cusomter and invoicing management and deliveries which has saved me 
-        more than 20 hours of work a month. It currently has over 90 clients loaded onto the system.
+        Last year I built and ERP (Enterprise Resource Planning) for my company's branch that we have
+        used to automate inventory, customer and invoicing management and deliveries which has saved us 
+        many hours of work a month. It currently has over 90 clients loaded onto the system.
         I have also written a React-Hooks library and published it to NPM.
         </p>
         <p>
